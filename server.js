34577d199c32c7147a4e6b7d155349f84acec347
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-09-20 06:56:18 -0700

    began setting up server.js and notes.json

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const { notes } = require('./data/notes');
 const express = require('express');
 
 //instantiate server
+const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-//make server listen, add routes
-app.get('/api/notes', (req, res) => {
-    res.send('Here are some notes');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// make server listen
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
-
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
-});
-
